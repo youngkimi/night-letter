2aeed5445b1ced64bf8d0f3b43d1f184e5bdd4cc
김영섭 <kimyoungseop33@gmail.com>
2024-07-24 17:40:57 +0900

    fix: DateTimeUtils, issuedAt, expiredAt

--- back-end/src/main/java/com/nightletter/domain/diary/repository/DiaryCustomRepositoryImpl.java
+++ back-end/src/main/java/com/nightletter/domain/diary/repository/DiaryCustomRepositoryImpl.java
@@ -178,7 +178,7 @@ public class DiaryCustomRepositoryImpl implements DiaryCustomRepository {
 			.from(diary)
 			.leftJoin(diary.diaryTarots, diaryTarot).fetchJoin()
 			.leftJoin(diaryTarot.tarot, tarot).fetchJoin()
-			.where(diary.writer.eq(member)
+			.where(diary.writer.memberId.eq(member.getMemberId())
 				.and(diary.date.between(request.getSttDate(), request.getEndDate())))
 			.orderBy(diary.date.asc())
 			.fetch();
